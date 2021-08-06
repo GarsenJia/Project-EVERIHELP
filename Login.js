240c06edc834fd70958c584ff8145b5ef803a6d6
Shengyi Jia <sjia6@u.rochester.edu>
2021-08-06 14:54:43 -0400

    1454EST update

--- Login.js
+++ Login.js
@@ -1,22 +1,28 @@
+var users = [];
+var paswrds = [];
+
 function li(){
-    var number = document.getElementById("number");
-    var paswrd = document.getElementById("paswrd");
-    var info = number.value + "," + paswrd.value;
+    var number = document.getElementById("number").value;
+    var paswrd = document.getElementById("paswrd").value;
+    var info = number + "," + paswrd + "\n";
     window.alert(info);
-    var fso = new ActiveXObject(Scripting.FileSystemObject);
-    var f = fso.createtextfile("./Users.txt",2,true);
-    //window.alert(info);
-    f.writeLine(info);
-    f.close();
+    users.push(number);
+    paswrds.push(paswrd)
+    window.alert(users[0]);
+    const fs = require('fs');
+    let data = "Learning how to write in a file.";
+    fs.writeFile('Output.txt', data, (err) => {
+        if (err) throw err;
+    })
 }
 
 function validusr(){
-    var number = document.getElementById("number");
-    var usrs = [];
-    var fso = new ActiveXObject(Scripting.FileSystemObject);
-    var f = fso.createtextfile("C:/Users/Shengyi Jia/IdeaProjects/Help/Users.txt",2,true);
-    while (!f.AtEndOfStream){
-        usrs.push(f.Readline());
+    window.alert("alert");
+    var number = document.getElementById("number").value;
+    window.alert(users.includes(number));
+    if(users.includes(number)){
+        window.alert("exist");
+    }else{
+        li();
     }
-
 }
