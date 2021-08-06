412da4f49682f414d8b21e0694784aa5e6cef3d6
Shengyi Jia <sjia6@u.rochester.edu>
2021-08-06 16:53:13 -0400

    1653EST login demo

--- Login.js
+++ Login.js
@@ -1,8 +1,6 @@
 var users = [];
 var paswrds = [];
 
-
-
 const submit = (ev)=>{
     var number = document.getElementById("usrname").value;
     var paswrd = document.getElementById("paswrd").value;
@@ -13,20 +11,33 @@ const submit = (ev)=>{
         document.forms[0].reset();
         //console.warn('added', {users});
         window.alert("已自动注册");
-        //jump to page #2
         window.alert("jump");
+        //jump
+        window.location.href="./Scratch.html";
+        const fs = require('fs')
+        let data = "Learning how to write in a file."
+        fs.writeFile('./Users.txt', data, (err) => {
+            if (err) throw err;})
     }else{
         index = users.indexOf(number);
         if(paswrds[index] == paswrd){
-            //jump to page #2
+            window.location.href="./Scratch.html";
             window.alert("jump");
         }else{
             window.alert("用户名或密码错误");
         }
     }
+
 }
+
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('btn').addEventListener('click', submit);
 })
 
-
+function validNumber(){
+    var number = document.getElementById("usrname").value;
+    var pattern = /^\d{11}$/;
+    if(!pattern.test(number) || numer == "" || number == undefined){
+        window.alert("请输入有效手机号")
+    }
+}
