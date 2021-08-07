f3f01048ea1452aba8c2f803107212437cac55ba
Shengyi Jia <sjia6@u.rochester.edu>
2021-08-06 21:39:04 -0400

    2138EST cannot jump

--- Login.js
+++ Login.js
@@ -1,43 +1,49 @@
-var users = [];
+var usernames = [];
 var paswrds = [];
 
+function contains(array,e){
+    for(i = 0;i < array.length; i++){
+        if(array[i] == e){
+            return true;
+        }
+    }
+    return false;
+}
 function ie9(){
-    window.alert("ok")
+    //window.alert("ok")
     var number = document.getElementById("usrname").value;
     var paswrd = document.getElementById("paswrd").value;
     var fso = new ActiveXObject("Scripting.FileSystemObject");
     var usrFile = fso.OpenTextFile("F:/CSC/Github/Unihack2021--33/Users.txt", 1, true);
     var psdFile = fso.OpenTextFile("F:/CSC/Github/Unihack2021--33/Passwords.txt", 1, true);
     while(!usrFile.AtEndOfStream){
-        window.alert("yup");
-        users.push(usrFile.ReadLine());
+        usernames.push(usrFile.ReadLine());
     }
     while(!psdFile.AtEndOfStream){
-        window.alert("yup");
         paswrds.push(psdFile.ReadLine());
     }
-    //usrFile.close();
-    //psdFile.close();
-    window.alert(number);
-    window.alert(users);
-    console.warn(users.includes(number));
-    if(!users.includes(number)){
-        window.alert("wrinting");
+    usrFile.Close();
+    psdFile.Close();
+    window.alert(contains(usernames,number));
+
+    if(!contains(usernames,number)){
+        window.alert("writing");
         var usrWrt = fso.OpenTextFile("F:/CSC/Github/Unihack2021--33/Users.txt", 8, true);
         var psdWrt = fso.OpenTextFile("F:/CSC/Github/Unihack2021--33/Passwords.txt", 8, true);
         usrWrt.WriteLine(number);
         psdWrt.WriteLine(paswrd);
         window.alert("已自动注册");
         window.alert("jump");
-        //jump
-        window.location.href="./Scratch.html";
-        usrWrt.close();
-        paswrd.close();
+        window.location.href="Scratch.html";
+        usrWrt.Close();
+        paswrd.Close();
+        window.alert("jump complete");
     }else{
-        index = users.indexOf(number);
+        index = usernames.indexOf(number);
         if(paswrds[index] == paswrd){
-            window.location.href="./Scratch.html";
             window.alert("jump");
+            window.location.href="Scratch.html";
+            window.alert("jump complete");
         }else{
             document.getElementById("paswrd").innerHTML="";
             window.alert("密码错误");
@@ -46,11 +52,6 @@ function ie9(){
 
 }
 
-/*
-document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('btn').addEventListener('click', submit);
-})
- */
 
 
 function validNumber(){
